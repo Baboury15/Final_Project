118f439d98f8add2e5653c59afa5e0d3ecf7bd49
Artjola <artjolameli@gmail.com>
2019-06-18 12:13:10 -0400

    Created a dropdown menu

--- src/frontend/dropdown.js
+++ src/frontend/dropdown.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+<div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="#">Brooklyn</a>
+    <a class="dropdown-item" href="#">Bronx</a>
+    <a class="dropdown-item" href="#">Manhattan</a>
+    <a class="dropdown-item" href="#">Staten Island</a>
+    <a class="dropdown-item" href="#">Queens</a>
+
+  </div>
+</div>
